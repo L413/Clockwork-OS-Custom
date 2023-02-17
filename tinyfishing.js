64cc57e330686d76215570101f3fa36adf8af158
L413 <53121093+L413@users.noreply.github.com>
2023-02-17 09:02:03 -0500

    Create tinyfishing.js

---
+++ tinyfishing.js
@@ -0,0 +1,32 @@
+function start() {
+  
+  ////MADE FOR VERSION 0.12.0 SCRIPT SYSTEM////
+
+  ////CONFIG////
+
+  var link = "https://raw.githubusercontent.com/PurePro4561/Tiny-Fishing/main/index.html"; // link to app webpage
+  var title = "Notepad"; // title shown in navbar
+  var id = "https://raw.githubusercontent.com/PurePro4561/Tiny-Fishing/main/html5game/TinyFishing.js"; // set this to the url location of your script
+  
+  ////SCRIPT////
+  
+  var ExampleLink = document.createElement("a");
+  var ExampleIFrame = document.createElement("iframe");
+  
+  ExampleIFrame.style = "display: none;";
+  ExampleIFrame.className = "app "+id;
+  ExampleIFrame.id = id;
+  ExampleIFrame.src = "about:blank";
+  
+  ExampleLink.href = "javascript:openapp('"+ ExampleIFrame.id +"','" + link + "');" ;
+  ExampleLink.innerHTML = title; 
+  ExampleLink.className = id; 
+  ExampleLink.addEventListener('dblclick', (e) => {
+    uninstallApp(ExampleIFrame.id);
+  });
+  
+  document.getElementById("navbar").appendChild(ExampleLink);
+  document.getElementById("main").appendChild(ExampleIFrame);
+}
+
+start();
