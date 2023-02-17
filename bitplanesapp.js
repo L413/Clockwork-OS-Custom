57fb6af4576094fec497cff90d883e3350bec90d
L413 <53121093+L413@users.noreply.github.com>
2023-02-17 12:41:51 -0500

    Create bitplanesapp.js

---
+++ bitplanesapp.js
@@ -0,0 +1,32 @@
+function start() {
+  
+  ////MADE FOR VERSION 0.12.0 SCRIPT SYSTEM////
+
+  ////CONFIG////
+
+  var link = "https://v6p9d9t4.ssl.hwcdn.net/html/2713283/dist/index.html"; // link to app webpage
+  var title = "Bit Planes"; // title shown in navbar
+  var id = "https://l413.github.io/Clockworks-OS-Custom-Apps/bitplanesapp.js"; // set this to the url location of your script
+  
+  ////SCRIPT////
+  
+  var ExampleLink = document.createElement("a");
+  var ExampleIFrame = document.createElement("iframe");
+  
+  ExampleIFrame.style = "display: none;";
+  ExampleIFrame.className = "app "+id;
+  ExampleIFrame.id = id;
+  ExampleIFrame.src = "about:blank";
+  
+  ExampleLink.href = "javascript:openapp('"+ ExampleIFrame.id +"','" + link + "');" ;
+  ExampleLink.innerHTML = title; 
+  ExampleLink.className = id; 
+  ExampleLink.addEventListener('dblclick', (e) => {
+    uninstallApp(ExampleIFrame.id);
+  });
+  
+  document.getElementById("navbar").appendChild(ExampleLink);
+  document.getElementById("main").appendChild(ExampleIFrame);
+}
+
+start();
