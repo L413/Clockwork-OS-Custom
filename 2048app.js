0919e2582726ecced4750a1681955792e36ef178
L413 <53121093+L413@users.noreply.github.com>
2023-02-19 17:18:40 -0500

    Create 2048app.js

---
+++ 2048app.js
@@ -0,0 +1,32 @@
+function start() {
+  
+  ////MADE FOR VERSION 0.12.0 SCRIPT SYSTEM////
+
+  ////CONFIG////
+
+  var link = "http://dawid.myslak.me/2048/index.html"; // link to app webpage
+  var title = "2048"; // title shown in navbar
+  var id = "https://l413.github.io/Clockworks-OS-Custom/2048app.js"; // set this to the url location of your script
+  
+  ////SCRIPT////
+  
+  var ExampleLink = document.createElement("a");
+  var ExampleIFrame = document.createElement("iframe");
+  
+  ExampleIFrame.style = "display: none;";
+  ExampleIFrame.className = "app "+id;
+  ExampleIFrame.id = id;
+  ExampleIFrame.src = "about:blank";
+  
+  ExampleLink.href = "javascript:openapp('"+ ExampleIFrame.id +"','" + link + "');" ;
+  ExampleLink.innerHTML = title; 
+  ExampleLink.className = id; 
+  ExampleLink.addEventListener('dblclick', (e) => {
+    uninstallApp(ExampleIFrame.id);
+  });
+  
+  document.getElementById("navbar").appendChild(ExampleLink);
+  document.getElementById("main").appendChild(ExampleIFrame);
+}
+
+start();
