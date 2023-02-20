38db84bbdfef4752a4c63a02f89ee1446a626487
L413 <53121093+L413@users.noreply.github.com>
2023-02-20 16:39:38 -0500

    Create mandalinapp.js

---
+++ mandalinapp.js
@@ -0,0 +1,32 @@
+function start() {
+  
+  ////MADE FOR VERSION 0.12.0 SCRIPT SYSTEM////
+
+  ////CONFIG////
+
+  var link = "https://7f65203c-4b3e-43bf-bda7-0c7e1aafd5d2.id.repl.co/madalin-stunt-cars-2/index.html"; // link to app webpage
+  var title = "MSC2"; // title shown in navbar
+  var id = "https://l413.github.io/Clockwork-OS-Custom/mandalinapp.js"; // set this to the url location of your script
+  
+  ////SCRIPT////
+  
+  var ExampleLink = document.createElement("a");
+  var ExampleIFrame = document.createElement("iframe");
+  
+  ExampleIFrame.style = "display: none;";
+  ExampleIFrame.className = "app "+id;
+  ExampleIFrame.id = id;
+  ExampleIFrame.src = "about:blank";
+  
+  ExampleLink.href = "javascript:openapp('"+ ExampleIFrame.id +"','" + link + "');" ;
+  ExampleLink.innerHTML = title; 
+  ExampleLink.className = id; 
+  ExampleLink.addEventListener('dblclick', (e) => {
+    uninstallApp(ExampleIFrame.id);
+  });
+  
+  document.getElementById("navbar").appendChild(ExampleLink);
+  document.getElementById("main").appendChild(ExampleIFrame);
+}
+
+start();
