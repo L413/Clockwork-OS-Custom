a93f7b2260def3c0e0686ef80dc98cee00e8b570
L413 <53121093+L413@users.noreply.github.com>
2023-02-20 18:26:13 -0500

    Create 1v1lolapp.js

---
+++ 1v1lolapp.js
@@ -0,0 +1,32 @@
+function start() {
+  
+  ////MADE FOR VERSION 0.12.0 SCRIPT SYSTEM////
+
+  ////CONFIG////
+
+  var link = "https://3kho.github.io/files/1v1lol/index.html"; // link to app webpage
+  var title = "PaperIO"; // title shown in navbar
+  var id = "https://l413.github.io/Clockwork-OS-Custom/1v1lolapp.js"; // set this to the url location of your script
+  
+  ////SCRIPT////
+  
+  var ExampleLink = document.createElement("a");
+  var ExampleIFrame = document.createElement("iframe");
+  
+  ExampleIFrame.style = "display: none;";
+  ExampleIFrame.className = "app "+id;
+  ExampleIFrame.id = id;
+  ExampleIFrame.src = "about:blank";
+  
+  ExampleLink.href = "javascript:openapp('"+ ExampleIFrame.id +"','" + link + "');" ;
+  ExampleLink.innerHTML = title; 
+  ExampleLink.className = id; 
+  ExampleLink.addEventListener('dblclick', (e) => {
+    uninstallApp(ExampleIFrame.id);
+  });
+  
+  document.getElementById("navbar").appendChild(ExampleLink);
+  document.getElementById("main").appendChild(ExampleIFrame);
+}
+
+start();
