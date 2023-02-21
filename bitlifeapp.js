ea8cf59f6faa712145b30126d7139e10c9c202d7
L413 <53121093+L413@users.noreply.github.com>
2023-02-21 08:43:40 -0500

    Update bitlifeapp.js

--- bitlifeapp.js
+++ bitlifeapp.js
@@ -5,7 +5,7 @@ function start() {
   ////CONFIG////
 
   var link = "https://6d302b9c-996d-4e68-829d-a18c5d1e27ae.id.repl.co/"; // link to app webpage
-  var title = "1v1.lol"; // title shown in navbar
+  var title = "Bitlife"; // title shown in navbar
   var id = "https://l413.github.io/Clockwork-OS-Custom/bitlifeapp.js"; // set this to the url location of your script
   
   ////SCRIPT////
