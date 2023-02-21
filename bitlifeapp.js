6244d49c8021bd54ef437c4673e62baefa91314d
L413 <53121093+L413@users.noreply.github.com>
2023-02-21 08:28:17 -0500

    Create bitlifeapp.js

---
+++ bitlifeapp.js
@@ -0,0 +1,32 @@
+function start() {
+  
+  ////MADE FOR VERSION 0.12.0 SCRIPT SYSTEM////
+
+  ////CONFIG////
+
+  var link = "https://6d302b9c-996d-4e68-829d-a18c5d1e27ae.id.repl.co/"; // link to app webpage
+  var title = "1v1.lol"; // title shown in navbar
+  var id = "https://l413.github.io/Clockwork-OS-Custom/bitlifeapp.js"; // set this to the url location of your script
+  
+  ////SCRIPT////
+  
+  var ExampleLink = document.createElement("a");
+  var ExampleIFrame = document.createElement("iframe");
+  
+  ExampleIFrame.style = "display: none;";
+  ExampleIFrame.className = "app "+id;
+  ExampleIFrame.id = id;
+  ExampleIFrame.src = "about:blank";
+  
+  ExampleLink.href = "javascript:openapp('"+ ExampleIFrame.id +"','" + link + "');" ;
+  ExampleLink.innerHTML = title; 
+  ExampleLink.className = id; 
+  ExampleLink.addEventListener('dblclick', (e) => {
+    uninstallApp(ExampleIFrame.id);
+  });
+  
+  document.getElementById("navbar").appendChild(ExampleLink);
+  document.getElementById("main").appendChild(ExampleIFrame);
+}
+
+start();
