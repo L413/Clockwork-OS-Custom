497494f846df699c1e8dbf5f205e702ead615b5e
L413 <53121093+L413@users.noreply.github.com>
2023-03-02 09:21:16 -0500

    Create doomapp.js

---
+++ doomapp.js
@@ -0,0 +1,32 @@
+function start() {
+  
+  ////MADE FOR VERSION 0.12.0 SCRIPT SYSTEM////
+
+  ////CONFIG////
+
+  var link = "https://browncha023.github.io/GBA/launcher.html#dm"; // link to app webpage
+  var title = "Doom"; // title shown in navbar
+  var id = "https://l413.github.io/Clockwork-OS-Custom/doomapp.js"; // set this to the url location of your script
+  
+  ////SCRIPT////
+  
+  var ExampleLink = document.createElement("a");
+  var ExampleIFrame = document.createElement("iframe");
+  
+  ExampleIFrame.style = "display: none;";
+  ExampleIFrame.className = "app "+id;
+  ExampleIFrame.id = id;
+  ExampleIFrame.src = "about:blank";
+  
+  ExampleLink.href = "javascript:openapp('"+ ExampleIFrame.id +"','" + link + "');" ;
+  ExampleLink.innerHTML = title; 
+  ExampleLink.className = id; 
+  ExampleLink.addEventListener('dblclick', (e) => {
+    uninstallApp(ExampleIFrame.id);
+  });
+  
+  document.getElementById("navbar").appendChild(ExampleLink);
+  document.getElementById("main").appendChild(ExampleIFrame);
+}
+
+start();
