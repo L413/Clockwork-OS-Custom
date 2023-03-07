8b9a399a4b55a0aeb0948f2db2f7723f19d16a83
L413 <53121093+L413@users.noreply.github.com>
2023-03-07 09:45:57 -0500

    Create backroomsapp.js

---
+++ backroomsapp.js
@@ -0,0 +1,36 @@
+function start() {
+  
+  ////MADE FOR VERSION 0.14.0 SCRIPT SYSTEM////
+
+  ////CONFIG////
+
+  var link = "https://l413.github.io/unblockedgames/play/backrooms/index.html"; // link to app webpage
+  var title = "Backrooms"; // title shown in navbar
+  var id = "https://l413.github.io/Clockwork-OS-Custom/backroomsapp.js"; // set this to the url location of your script
+  
+  ////SCRIPT////
+  
+  var ExampleLink = document.createElement("a");
+  var ExampleIFrame = document.createElement("iframe");
+  
+  ExampleIFrame.style = "display: none;";
+  ExampleIFrame.className = "app "+id;
+  ExampleIFrame.id = id;
+  ExampleIFrame.src = "about:blank";
+  
+  ExampleLink.href = "javascript:openapp('"+ ExampleIFrame.id +"','" + link + "');" ;
+  ExampleLink.innerHTML = title; 
+  ExampleLink.className = id; 
+  ExampleLink.addEventListener('dblclick', (e) => {
+    uninstallApp(ExampleIFrame.id);
+  });
+  ExampleLink.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    closeApp(ExampleIFrame.id);
+  });
+  
+  document.getElementById("navbar").appendChild(ExampleLink);
+  document.getElementById("main").appendChild(ExampleIFrame);
+}
+
+start();
